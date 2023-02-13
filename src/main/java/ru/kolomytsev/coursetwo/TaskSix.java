c7dfb6ccd7cec67e5bd169b254ca9b299b809ce4
akolomytsev <kolm.87@mail.ru>
2023-02-13 22:19:08 +0600

    Сделал второе задание третьего урока

--- src/main/java/ru/kolomytsev/coursetwo/TaskSix.java
+++ src/main/java/ru/kolomytsev/coursetwo/TaskSix.java
@@ -10,46 +10,43 @@ import java.util.Arrays;
 public class TaskSix {
     public static void main(String[] args) {
         int[] g = {5, 5, 5, 15};
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < g.length-1; i++) {
-            a += g[i];
-            //b += g[g.length + i-3];
-            //if (a == b) return true;
-            //b = g[1] + g[2] + g[3];
-            System.out.print(a + " ");
-
+        for (int i = 0; i < g.length ; i++) {
+            int a = 0;
+            int b = 0;
+            for (int j = 0; j < i ; j++) {
+                a += g[j];
+                System.out.print(a +"t" + " ");
+            }
+            System.out.println("g");
+            for (int j = i; j < g.length; j++) {
+                b += g[j];
+                System.out.print(b + "b" + " ");
+            }
+            if (a == b){
+                System.out.println("Жопа" + a + " " + b);
+            }
         }
-        System.out.println();
-        for (int j = 1; j < g.length; j++) {
-            b = Arrays.stream(g[j]).sum();
-            System.out.print(b + " ");
-        }
-        if (a == b){
-            System.out.println("Жопа");
+        System.out.println(checkBalance(g));
+    }
+    static boolean checkBalance ( int[] g){
+        int leftsum, rightsum;
+        for (int i = 0; i < g.length; i++) {
+            leftsum = 0;
+            rightsum = 0;
+            for (int j = 0; j < i; j++) {
+                leftsum += g[j];
+                System.out.print(leftsum + "m ");
+            }
+            System.out.println("l");
+            for (int j = i; j < g.length; j++) {
+                rightsum += g[j];
+                System.out.print(rightsum + "t ");
+            }
+            if (leftsum == rightsum){
+                return true;
+            }
         }
 
-
-//        System.out.println(checkBalance(g));
-//    }
-//    static boolean checkBalance ( int[] g){
-//        int leftsum, rightsum;
-//        for (int i = 0; i < g.length; i++) {
-//            leftsum = 0;
-//            rightsum = 0;
-//            for (int j = 0; j < i; j++) {
-//                leftsum += g[j];
-//                System.out.print(leftsum + "m ");
-//            }
-//            System.out.println("l");
-//            for (int j = i; j < g.length; j++) {
-//                rightsum += g[j];
-//                System.out.print(rightsum + "t ");
-//            }
-//            if (leftsum == rightsum) return true;
-//            System.out.println("l");
-//        }
-//
-//        return false;
+        return false;
     }
 }
